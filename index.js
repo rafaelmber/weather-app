22b3b998f6c6ee1e93a55cb9703c819cc4b3b7fd
Rafael Martinez Bermudez <rafaelmartinez2699@gmail.com>
2024-09-06 22:30:44 -0500

    Update view according to ajax request

--- index.js
+++ index.js
@@ -15,7 +15,26 @@ searchForm.addEventListener('submit', function (event) {
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      const result = data['result'];
+      showWeather(result);
     })
-    .catch((error) => console.log(error));
+    .catch((error) => console.log('Error:', error));
 });
+
+function showWeather(weather_data) {
+  const location = document.getElementById('current_weather__location');
+  console.log(weather_data.name);
+  location.innerText = weather_data['name'];
+
+  const weatherDescription = document.getElementById('current-weather__description');
+  weatherDescription.innerText = `${weather_data['weather'][0]['main']}: ${weather_data['weather'][0]['description']}`;
+
+  const temperature = document.getElementById('current_weather__temperature');
+  temperature.innerText = weather_data['main']['temp'] + '°C';
+
+  const moreTemp = document.getElementById('current_weather__more_temp');
+  moreTemp.innerText = `${weather_data['main']['temp_min']}° | ${weather_data['main']['temp_max']}°. Sensación térmica: ${weather_data['main']['feels_like']}°`;
+
+  const weatherIcon = document.getElementById('current-weather__icon');
+  weatherIcon.src = `https://openweathermap.org/img/wn/${weather_data['weather'][0]['icon']}@2x.png`;
+}
